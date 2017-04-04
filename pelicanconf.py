bad867b1808490e72abc6c745b09efc66d54c3c2
40423252 <40423252@gm.nfu.edu.tw>
2017-04-04 21:30:54 +0800

    live

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = 'KMOL'
-SITENAME = '2017Spring 協同產品設計實習 (虎尾科大MDE)'
+AUTHOR = '40423252'
+SITENAME = '40423252 上課作業'
 # 不要用文章所在目錄作為類別
 USE_FOLDER_AS_CATEGORY = False
 
